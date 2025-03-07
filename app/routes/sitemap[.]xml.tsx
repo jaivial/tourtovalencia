6c8c0c0d4a711e52c655d70b669340a4ebb222a4
jaivial <114494844+jaivial@users.noreply.github.com>
2025-03-07 17:46:40 +0100

    I apolog

---
+++ app/routes/sitemap[.]xml.tsx
@@ -0,0 +1,62 @@
+import type { LoaderFunction } from "@remix-run/node";
+
+export const loader: LoaderFunction = async () => {
+  // Create the XML content as a string
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <!-- Homepage -->
+  <url>
+    <loc>https://tourtovalencia.com/</loc>
+    <lastmod>2025-03-07</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  
+  <!-- Main Routes -->
+  <url>
+    <loc>https://tourtovalencia.com/valencia-things-to-do</loc>
+    <lastmod>2025-03-07</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  
+  <url>
+    <loc>https://tourtovalencia.com/book</loc>
+    <lastmod>2025-03-07</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  
+  <url>
+    <loc>https://tourtovalencia.com/pages/cuevas-de-san-jose</loc>
+    <lastmod>2025-03-07</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  
+  <url>
+    <loc>https://tourtovalencia.com/pages/experiencia-de-vinos</loc>
+    <lastmod>2025-03-07</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  
+  <url>
+    <loc>https://tourtovalencia.com/legal</loc>
+    <lastmod>2025-03-07</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+  </url>
+</urlset>`;
+
+  // Return the XML with the correct content type
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+      "Content-Length": String(Buffer.byteLength(xml)),
+      "Cache-Control": "public, max-age=3600" // Cache for 1 hour
+    }
+  });
+};
+
+// No need for a default export since this is just a resource route
